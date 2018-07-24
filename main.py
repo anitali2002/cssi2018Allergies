f8adbc815f6b5e5a904b84721a9e67a09b5f1515
Anita Li <anitali629@gmail.com>
2018-07-24 12:14:14 -0400

    edited AllergySubmitPage handler
    wrote the script for the handler "AllergySubmitPage". creates a recipe object with the inputted information and adds to datastore

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import webapp2
 import os
 import jinja2
-from allergy import Allergy
-from recipe import Recipe
-# from google.appengine.api import urlfetch
+import ast
+from models import Recipe
+from models import Allergy
+from google.appengine.api import urlfetch
 
 theJinjaEnvironment = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions = [],
     autoescape = True)
 
-# finds and returns the allergy entity (with the allergy information)
+# formats a string to all lower case, spaces taken out, and separated (by comma) into a list
+def formatString(string):
+    formattedString = string.lower()
+    formattedString = formattedString.replace("; ", ";")
+    stringList = formattedString.split(";")
+    return stringList
+
+# Finds and returns the allergy entity (with the allergy information)
 # in the database of all allergens
 def allergySearch():
     allergyDatabase = Allergy.query().fetch()
@@ -21,8 +29,17 @@ def allergySearch():
             allergy = allergyDatabase[i]
     return allergy
 
-# def recipeFetch():
-#     urlfetch.fetch(url)
+# RecipePuppy API call. Returns a list of recipe dictionaries.
+# All recipes have "title", "href", "ingredients", and "thumbnail"
+def recipeFetch(ingredients = "", type = ""):
+    ingredients = ingredients.lower()
+    ingredients = ingredients.replace(" ", "")
+    type = type.lower()
+    recipeSearch = "http://www.recipepuppy.com/api/?i="+ ingredients + "&q=" + type
+    searchResult = urlfetch.fetch(recipeSearch)
+    searchResult = ast.literal_eval(searchResult.content)
+    recipes = searchResult["results"]
+    return recipes
 
 class WelcomePage(webapp2.RequestHandler):
     def get(self):
@@ -30,32 +47,56 @@ class WelcomePage(webapp2.RequestHandler):
         self.response.write(welcomeTemplate.render())
 
 class RecipeSubmitPage(webapp2.RequestHandler):
-    def get(self):
-        recipeSubmitTemplate = theJinjaEnvironment.get_template('templates/recipeSubmit.html')
-        self.response.write(recipeSubmitTemplate.render())
-
-class GenInfoPage(webapp2.RequestHandler):
-    def get(self):
-        genInfoTemplate = theJinjaEnvironment.get_template('templates/genInfo.html')
-        self.response.write(genInfoTemplate.render())
-
-class AllergyInfoPage(webapp2.RequestHandler):
-    def get(self):
-        allergyInfoTemplate = theJinjaEnvironment.get_template('templates/allergyInfo.html')
-        self.response.write(allergyInfoTemplate.render())
     def post(self):
-        recipeTemplate = theJinjaEnvironment.get_template('templates/recipe.html')
-        self.response.write(recipeTemplate.render())
+        recipeSubmitTemplate = theJinjaEnvironment.get_template('templates/recipeSubmit.html')
+        title = self.response.get("title")
+        link = self.response.get("link")
+        image = self.response.get("image")
+        allergensFree = self.response.get("allergensFree") #list
+        allergensFree = formatString(allergensFree)
+        ingredients = self.response.get("ingredients") #list
+        ingredients = formatString(ingredients)
+        otherTags = self.response.get("otherTags") #list
+        otherTags = formatString(otherTags)
+        steps = self.response.get("steps") #list
+        steps = formatString(steps)
 
-class AllergySubmitPage(webapp2.RequestHandler):
-    def get(self):
-        allergySubmitTemplate = theJinjaEnvironment.get_template('templates/allergySubmit.html')
-        self.response.write(allergySubmitTemplate.render())
+        recipe = Recipe(title = title, link = link, image = image)
+        recipe.put()
+        for allergen in allergensFree:
+            recipe.allergensFree.append(allergen)
+        for ingredient in ingredients:
+            recipe.ingredients.append(ingredient)
+        for otherTag in otherTags:
+            recipe.otherTags.append(otherTag)
+        for step in steps:
+            recipe.steps.append(step)
+
+# class GenInfoPage(webapp2.RequestHandler):
+#     def get(self):
+#         genInfoTemplate = theJinjaEnvironment.get_template('templates/genInfo.html')
+#         self.response.write(genInfoTemplate.render())
+#
+# class AllergyInfoPage(webapp2.RequestHandler):
+#     def get(self):
+#         allergyInfoTemplate = theJinjaEnvironment.get_template('templates/allergyInfo.html')
+#         self.response.write(allergyInfoTemplate.render())
+# # posts the selected recipe- if there is a link, goes to the link. if not, go to recipe html template
+#     def post(self):
+#         recipeTemplate = theJinjaEnvironment.get_template('templates/recipe.html')
+#         allergyName = self.request.get("allergyName")
+#
+#         self.response.write(recipeTemplate.render())
+#
+# class AllergySubmitPage(webapp2.RequestHandler):
+#     def get(self):
+#         allergySubmitTemplate = theJinjaEnvironment.get_template('templates/allergySubmit.html')
+#         self.response.write(allergySubmitTemplate.render())
 
 app = webapp2.WSGIApplication([
     ('/', WelcomePage),
     ('/submitRecipe', RecipeSubmitPage),
-    ('/genInfo', GenInfoPage),
-    ('/allergyInfo', AllergyInfoPage),
-    ('/submitAllergy', AllergySubmitPage),
+    # ('/genInfo', GenInfoPage),
+    # ('/allergyInfo', AllergyInfoPage),
+    # ('/submitAllergy', AllergySubmitPage),
 ], debug=True)
--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from google.appengine.ext import ndb
 
+class Allergy(ndb.Model):
+    allergy = ndb.StringProperty(required = True)
+    symptoms = ndb.StringProperty(repeated = True)
+    toAvoid = ndb.StringProperty()
+
 class Recipe(ndb.Model):
     title = ndb.StringProperty(required = True)
     link = ndb.StringProperty()
     image = ndb.StringProperty()
-    tags = ndb.StringProperty(repeated = True)
-    allergens = ndb.StringProperty(repeated = True)
+    allergensFree = ndb.StringProperty(repeated = True)
     ingredients = ndb.StringProperty(repeated = True)
     steps = ndb.StringProperty(repeated = True)
+    otherTags = ndb.StringProperty(repeated = True)
